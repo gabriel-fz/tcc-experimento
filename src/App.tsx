59d9e76b64e5e6de8eff8d3c3e186948eaeb3b38
gabrielfiorese <gabriel@inovahu.com>
2021-04-20 00:13:10 -0300

    adicionando estilização com styled-components e criando estilos e temas globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+
+import GlobalStyle from './styles/global';
+import theme from './styles/theme';
 
 import Routes from './routes';
 
 const App: React.FC = () => (
   <BrowserRouter>
-    <Routes />
+    <ThemeProvider theme={theme}>
+      <Routes />
+
+      <GlobalStyle />
+    </ThemeProvider>
   </BrowserRouter>
 );
 
---
+++ src/styles/theme/colors.ts
@@ -0,0 +1,9 @@
+export const colors = {
+  primary: '#141C3C',
+  secondary: '#212D5B',
+  tertiary: '#243B97',
+  success: '#1AD67B',
+  danger: '#FF535A',
+  play: '#D32EFF',
+  normal: '#2B8CFA',
+};
