acee3c2036d5fdc35a18bd75b5b194dd991ad9c9
gabrielfiorese <gabriel@inovahu.com>
2021-05-01 11:55:27 -0300

    concluindo a pagina de erro 404

--- src/pages/Error404/index.tsx
+++ src/pages/Error404/index.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import imgError from '../../assets/404.svg';
+import ButtonLink from '../../components/ButtonLink';
+
+import { Container } from './styles';
 
 const Error404: React.FC = () => {
-  return <h1>Erro 404</h1>;
+  return (
+    <Container>
+      <img src={imgError} alt="imagem de erro" />
+
+      <h1>Página não encontrada</h1>
+
+      <ButtonLink to="/" color="play">
+        Voltar para o início
+      </ButtonLink>
+    </Container>
+  );
 };
 
 export default Error404;
